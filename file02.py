ddefaf724b23f285036a8d1d8f98ff403ff87059
BurakE-Arik <burakar1912@gmail.com>
2026-04-27 13:55:58 +0300

    Unnecessary function deleted in file02

--- file02.py
+++ file02.py
@@ -4,25 +4,14 @@ alphabet_lower_case = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","
 alphabet_upper_case = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","V","Y","Z","X","W"]
 
 
-def case_sensitive_counter(word,char):
+def char_counter(word,char):
     char_count = 0
     
     for i in word:
         if char == i:
             char_count += 1
             
-    return f"{char} char found {char_count} times in {word}"
-
-def not_case_sensitive_counter(word,char):
-    char_count = 0
-    
-    word, char = word.lower(), char.lower()
-    
-    for i in word:
-        if char == i:
-            char_count += 1
-            
-    return f"{char} char found {char_count} times in {word}"
+    return f"'{char}' char found /{char_count}/ times in {word}"
 
 if __name__ == "__main__":
     print("=== Char Counter ===")
@@ -36,9 +25,10 @@ if __name__ == "__main__":
     word, char = word.strip(), char.strip()
     
     if answer == "y":
-        print(case_sensitive_counter(word,char))
+        print(char_counter(word,char))
     elif answer == "n":
-        print(not_case_sensitive_counter(word,char))
+        word, char = word.lower(), char.lower()
+        print(char_counter(word,char))
     else:
         print("Something went wrong")
         
